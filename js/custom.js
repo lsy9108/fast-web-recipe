7e147ec44b67780d939e492c8e0f1cdf04124990
soggii <lsy9108@gmail.com>
2017-10-20 18:21:39 +0900

    edit img in html To bgimg in css
    bg img done, need to fix slide in js

--- js/custom.js
+++ js/custom.js
@@ -1,17 +1,7 @@
 ﻿$(document).ready(function(){       
 	/* on load */
+    
     /*
-    
-    $(window).load(function(){
-        alert('테스트');
-    });
-    
-    
-    $("#id").load(function(){
-        alert('테스트');
-    });
-    
-    */
     
     
     /* contact, home btn click event */
@@ -21,6 +11,35 @@
     $(".left2>.leftTop>.home a").on("click",function(){
         $(".left2").css({left:"-60%"});
     });
+    
+    
+    /* slider */
+    $(".slide li").each(function(aa){
+        var RightWid = parseInt($(this).parent(".right").Width());
+    $(this).css({left:aa*RightWid});
+    });
+    
+    var putimg = 0;
+    $('.slide li').each(function(aa){
+        $(this).css({left:putimg});
+        putimg += RightWid;
+    });
+    
+    setInterval(function(){
+        $(".slide li").each(function(bb){
+            var RightWid = parseInt($(this).Width());
+            var original = parseInt($(this).css('left')); 
+            
+            var moveTo = original-RightWid;
+            $(this).animate({'left':moveTo},2000,function(){ //콜백함수
+                
+                var moved = parseInt($(this).css('left'));
+                if(moved <= -RightWid){
+                    $(this).css('left', RightWid*2 +'px');
+                }
+            });
+        });
+    },2000)
     
 }); // the end
 
